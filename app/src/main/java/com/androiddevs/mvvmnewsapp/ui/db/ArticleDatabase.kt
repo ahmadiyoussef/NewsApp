859da52e1d7b0de2e0730b9f5d8d4646c8476f5c
Youssef AHMADI <ahmadiyoussef14@gmail.com>
2021-03-26 14:07:28 +0100

    first commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/db/ArticleDatabase.kt
@@ -0,0 +1,43 @@
+package com.androiddevs.mvvmnewsapp.ui.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.androiddevs.mvvmnewsapp.ui.models.Article
+
+@Database(entities = [Article::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class ArticleDatabase : RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: ArticleDatabase?=null
+
+        fun getInstance(context: Context): ArticleDatabase {
+
+            synchronized(this){
+                var instance = INSTANCE
+
+                if(instance==null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ArticleDatabase::class.java,
+                        "article_db.db"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE=instance
+                }
+
+                return instance
+
+            }
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "795ee76b7f054cee94c350496ef5ae25"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+    }
+}
